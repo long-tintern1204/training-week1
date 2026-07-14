d3e9ec7234e92d2b4e54cedcfead088cc749cee4
Bakaryuu <bakaryuu1997@gmail.com>
2026-07-14 09:37:21 +0900

    first commit

--- client.py
+++
@@ -1,7 +0,0 @@
-import requests
-res = requests.post(
-    "http://127.0.0.1:8001/items",
-    json={"name": "ao", "price" : 1000, "description": "ao thun" },            
-)
-print(res.status_code)
-print(res.text)
